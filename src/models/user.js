20b875d556014d800163d4a4ed32cee706c7392b
MustafaNova2 <me6499864@gmail.com>
2026-03-25 18:00:27 +0100

    Refactor Authentication & User Management
    * Simplified user model
    * Restricted user data to public information only
    * Improved password handling (stored as hashed values)
    * Implemented static method for user login (username-based)
    * Added JWT-based authentication
    * Implemented login and logout functionality
    * Enhanced logout: ability to remove all tokens for a specific user
    * Added endpoint to retrieve all users

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
+    },
     username: {
         type: String,
         unique: true,
@@ -37,16 +49,6 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    address: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Address'
-    },
-    meta: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'UserMeta'
-    },
     tokens: [{
         token: {
             type: String,
@@ -56,6 +58,44 @@ const userSchema = new mongoose.Schema({
 }, {
         timestamps: true
     })
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    if (!userObject.admin) delete userObject.admin
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'mySecret')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (username, password) => {
+
+    const user = await User.findOne({ username })
+    if (!user) throw new Error('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) throw new Error('Unable to login')
+
+    return user
+}
+
+// Hash the plain text password before save
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+})
 
 const User = mongoose.model('User', userSchema)
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,22 +1,61 @@
 const express = require('express')
 const User = require('../models/user')
-const UserMeta = require('../models/userMeta')
-const Address = require('../models/address')
+const auth = require('../middlewares/auth')
 
 const router = new express.Router()
 
-//Create a user
+// Create a user
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-    const address = new Address(req.body)
-    const userMeta = new UserMeta(req.body)
-    user.address = address._id
-    user.meta = userMeta._id
     try {
         await user.save()
-        await address.save()
-        await userMeta.save()
-        res.status(201).send({ user, userMeta, address })
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Login User
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.username, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Logout user
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Logout all
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', auth, async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
     } catch (e) {
         res.status(400).send(e)
     }
